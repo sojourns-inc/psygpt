93d7af44fa82b76e2ac97ff2782b3f01dc46e5b5
sernyl <lex@sojourns.io>
2023-08-23 13:17:51 -0400

    Update prompt for /info

--- psygptbot.py
+++ psygptbot.py
@@ -138,10 +138,10 @@ def fetch_dose_card_from_psygpt(substance_name: str, chat_id: str):
     try:
         raw = {
             "model": LLM_MODEL_ID,
-            "question": f"Generate a drug information card using html for {substance_name}.\n\n Example drug information card:\n\n"
+            "question": f"Generate a drug information card using html for {substance_name}. Respond only with the HTML code, as it is in the examle card.\n\n Example drug information card:\n\n"
             + create_drug_info_card(substance_name)
             + "\n\nNote: Not every section from the example dose card is required, and you may add additional sections if needed. Please keep the formatting compact and uniform using HTML.",
-            "temperature": "0.1",
+            "temperature": "0.2",
             "max_tokens": 10000,
         }
         return post_and_parse_url(f"{BASE_URL}/chat/{chat_id}/question", raw)
